263f4cbcb5136410f585f8e31b6ac3682dbb5fc7
ade bakari <redariez@gmail.com>
2019-12-11 16:01:46 -0800

    iterate through array

--- Arrays/js/app.js
+++ Arrays/js/app.js
@@ -13,3 +13,37 @@
      alert(phrase);
  }
  makePhrases();
+
+ // How to iterate over an array
+
+ var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69,
+               34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 
+               46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44,];
+               
+var output; 
+
+//While loop used to iterate through array
+var i = 0;
+while(i < scores.length) {
+    output = "Bubble solution #" + i + " score: " + scores[i];
+    console.log(output);
+    i = i +1;
+}
+
+//For loop used to iterate through array
+for (var i = 0; i < scores.length; i++) {
+    output = "Bubble solution #" + i + " score: " + scores[i];
+    console.log(output);
+}
+
+//adding if statement
+var highScore = 0;
+    for (var i = 0; i < scores.length; i++) {
+        output = "Bubble solution #" + i + " score: " + scores[i];
+        console.log(output);
+        if(scores[i] > highScore) {
+            highScore = scores[i];
+        }
+    }
+    console.log("BUbbles tests: " + scores.length );
+    console.log("Highest bubble score: " + highScore);
