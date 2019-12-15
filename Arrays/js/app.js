dfb524266e3472e153f2e3107d868b9dc9c79b21
ade bakari <redariez@gmail.com>
2019-12-15 10:43:28 -0800

    refactoring and adding functions

--- Arrays/js/app.js
+++ Arrays/js/app.js
@@ -47,3 +47,43 @@ var highScore = 0;
     }
     console.log("BUbbles tests: " + scores.length );
     console.log("Highest bubble score: " + highScore);
+
+//Putting it all together
+
+
+var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69,
+    34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 
+    46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44,];
+    
+var output; 
+
+function printAndGetHighScores(scores) {
+    var highScore = 0;
+    var output;
+    for (var i = 0; i < scores.length; i++) {
+        output = "Bubble solution #" + i + " score: " + scores[i];
+        console.log(output);
+        if (scores[i] > highScore) {
+            highScore = scores[i];
+        }
+    }
+    return highScore;
+}
+
+function getBestResults(scores, highscore) {
+    var bestSolutions = [];
+    for (var i = 0; i < scores.length; i++) {
+        if (scores[i] == highScore) {
+            bestSolutions.push[i];
+        }
+    }
+    return bestSolutions;
+}
+
+var highScore = printAndGetHighScores(scores);
+console.log("BUbbles tests: " + scores.length );
+console.log("Highest bubble score: " + highScore);
+
+var bestSolutions = getBestResults(scores, highScore);
+console.log("Solutions with highest score: " + bestSolutions);
+ 
